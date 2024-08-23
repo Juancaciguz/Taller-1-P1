a4047990b37f0d08b379aaabc02fa38989314cfc
Juan Carlos Citelly Guzmán <juancaciguz@hotmail.com>
2024-08-22 21:45:18 -0500

    Se agregaron gráficas con relación al año de las peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import matplotlib.pyplot as plt
+import matplotlib
+import io
+import urllib, base64
 
 from .models import Movie
 # Create your views here.
@@ -17,3 +21,42 @@ def home(request):
 def about(request):
     #return HttpResponse('<h1>Welcome to About Page</h1>')
     return render(request, 'about.html')
+
+def statistic_view(request):
+    matplotlib.use('Agg')
+    years = Movie.objects.values_list('year', flat=True).distinct().order_by('year')#obtener todos los años de las peliculas
+    movie_counts_by_year = {} #Crear un diccionario para almacenar la cantidad de películas por año
+    for year in years: #Contar la cantidad de películas por año
+        if year:
+            movies_in_year = Movie.objects.filter(year=year)
+        else:
+            movies_in_year= Movie.objects.filter(year__isnull=True)
+            year = "None"
+        count = movies_in_year.count()
+        movie_counts_by_year[year] = count
+    bar_width = 0.5
+    bar_spacing = 0.5
+    bar_positions = range(len(movie_counts_by_year))
+
+    #Crear gráfica de barras
+    plt.bar(bar_positions, movie_counts_by_year.values(), width=bar_width, align='center')
+    #Personalizar la gráfica
+    plt.title('Movies per year')
+    plt.xlabel('Year')
+    plt.ylabel('Number of movies')
+    plt.xticks(bar_positions, movie_counts_by_year.keys(), rotation=90)
+    #Ajustar el espaciado entre las barras
+    plt.subplots_adjust(bottom=0.3)
+    #Guardar la gráfica en un objeto BytesIO
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    plt.close()
+
+    #Convertir la gráfica a base64
+    image_png = buffer.getvalue()
+    buffer.close()
+    graphic = base64.b64decode(image_png)
+    graphic = graphic.decode('utf-8')
+    # Renderizar la plantilla statistics.html con la gráfica
+    return render(request, 'statistics.html', {'graphic': graphic})
